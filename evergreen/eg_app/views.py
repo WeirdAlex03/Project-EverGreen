24c51caa71038ec67e7a540e3232913d0c35d904
Sumanyu Janapareddy <111717258+sumanyuj@users.noreply.github.com>
2024-10-08 14:53:17 -0400

    added to views.py 2 functions to render css and js files. Also moved function.js and styles.css to different paths in the static folder, as per reqs

--- evergreen/eg_app/views.py
+++ evergreen/eg_app/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 # handles request 
 def index(request):
     return render(request,'index.html')
+def obj1_css(request):
+    return render(request, 'eg_app/static/css/styles.css') # this has to be in a different path other than index.html is
+def obj1_js(request):
+    return render(request,'eg_app/static/js/functions.js') # and so with this one!
