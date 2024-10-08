ba64baf47c82478dfd0dab96e414daf37f82d301
Sumanyu Janapareddy <111717258+sumanyuj@users.noreply.github.com>
2024-10-08 14:44:32 -0400

    changed settings.py to just BASE_DIR instead of looking at templates

--- evergreen/evergreen/settings.py
+++ evergreen/evergreen/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "evergreen.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
