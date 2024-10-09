019d08cf34908f9ac45ea46188127434d9867abb
Sumanyu Janapareddy <111717258+sumanyuj@users.noreply.github.com>
2024-10-09 00:11:04 -0400

    changed index.html to have django understand its static files and added image

--- evergreen/eg_app/views.py
+++ evergreen/eg_app/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 # handles request 
 def index(request):
     return render(request,'index.html')
-def obj1_css(request):
-    return render(request, 'eg_app/static/css/styles.css') # this has to be in a different path other than index.html is
-def obj1_js(request):
-    return render(request,'eg_app/static/js/functions.js') # and so with this one!
+
+
+
--- evergreen/evergreen/settings.py
+++ evergreen/evergreen/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
 ]
 
 ROOT_URLCONF = "evergreen.urls"
@@ -74,13 +75,14 @@ WSGI_APPLICATION = "evergreen.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -122,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Ensures JS, CSS and other static files get NOSNIFF
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
