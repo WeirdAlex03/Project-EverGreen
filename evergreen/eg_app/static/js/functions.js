2e3c2e75a4375a2a36fa01621619e8f1e693c760
Sumanyu Janapareddy <111717258+sumanyuj@users.noreply.github.com>
2024-10-12 16:16:35 -0400

    removed excess static files

--- evergreen/eg_app/static/js/functions.js
+++
@@ -1,7 +0,0 @@
-/* Simple JavaScript to "do something/anything"
- * Automatically triggered by onload
- */
-function start() {
-	document.getElementById("about").innerHTML += "<br/> We have Javascript, too! 😀"
-	document.getElementById("about").innerHTML += "<br/> This probably all gonna be replaced anyway idk"
-}
