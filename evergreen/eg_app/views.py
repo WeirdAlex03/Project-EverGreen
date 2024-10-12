ebb697681a00f8b927e3a266569f49afba39fe4b
rancescolon <109361303+rancescolon@users.noreply.github.com>
2024-10-12 18:20:12 -0400

    textHandler for paths

--- evergreen/eg_app/views.py
+++ evergreen/eg_app/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse, HttpResponseNotFound
+from django.template import loader
+import mimetypes
+from django.conf import settings
+
+
 
 # Create your views here.
 
 # handles request 
-def index(request):
-    return render(request,'index.html')
+
+def getFileType(filePath):
+    fileType = mimetypes.guess_type(filePath)
+    return fileType
+
+
+def indexHandler(request): 
+    template = str(loader.get_template('index.html'))
+    return HttpResponse(render(request,template))
+
+
+def textHandler(request):
+    fileType = getFileType(request.path)
+    if fileType in ['js', 'css', 'html']:
+        template = str(loader.get_template(request.path))
+
+        response = HttpResponse()
+
+        response['content'] = str(loader.get_template(request.path))
+        response['Content-Length'] = len(response.content)
+        response['content_type'] = fileType
+        response['X-Content-Type-Options'] = "nosniff"
+        
+        #Content-Length
+        #Content-Type
+        #X-Content-Type-Options: nosniff
+        #HTTP/1.1 200 OK
+
+    else:
+        return HttpResponseNotFound("HTTP/1.1 404 Not Found OK")
+            
+# def imgHandler(request):
