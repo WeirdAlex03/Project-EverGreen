a5c43cdd1fac3e97691aec056633e21373a7476f
Sumanyu Janapareddy <111717258+sumanyuj@users.noreply.github.com>
2024-10-23 21:33:30 -0400

    changed a few wordings

--- evergreen/eg_app/util/encrypt.py
+++ evergreen/eg_app/util/encrypt.py
@@ -8,6 +8,6 @@ def encrypt(plaintext):
     # encode plaintext string to bytes
     plaintext = plaintext.encode('')
 
-    bcrypted_plaintext = bcrypt.hashpw(plaintext, bcrypt.gensalt())
+    hashed_password = bcrypt.hashpw(plaintext, bcrypt.gensalt())
 
-    return bcrypted_plaintext
+    return hashed_password
