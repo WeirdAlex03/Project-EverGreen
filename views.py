6b7a3c774f85c08d5e48aa57ae9f974a2879d28f
rancescolon <109361303+rancescolon@users.noreply.github.com>
2024-10-24 18:02:38 -0400

    Add files via upload

---
+++ views.py
@@ -0,0 +1,61 @@
+# Create your views here
+#views are just the handler for shit that will be pass into urls.py
+
+
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from django.conf import settings
+import mimetypes
+from pathlib import Path
+from urllib.parse import quote
+
+
+
+def getFileType(filePath):
+    return mimetypes.guess_type(filePath)[0]  
+
+def indexHandler(request):
+    return render(request, 'evergreen/staticfiles/staticfiles/index.html')
+
+def fileHandler(request, fileName): # can handle img and text 
+    # sanitizedfileName = quote(fileName) # get a clean full file path 
+    # path = Path(settings.STATIC_ROOT) / sanitizedfileName
+
+    path = fileName
+
+    allowedType = {'.css', '.html', '.js', '.png', '.jpg', '.jpeg', '.gif', '.mp3', '.mp4', '.xml', '.json', '.pdf','.ico'} # can add more, 
+
+    if not path.suffix.lower() in allowedType: # to deal with user uploads 
+        return HttpResponseNotFound("404 - File type not allowed")
+    
+    # #deal with /../ attacks 
+    # rootPath = Path(settings.STATIC_ROOT).resolve() #.resolve get absolute path 
+    # if not path.resolve().is_relative_to(rootPath):
+    #     return HttpResponseNotFound("404 - Invalid file path")
+    
+    if path.exists() and path.is_file(): # make sure its not a directory 
+        contentType = getFileType(path)
+        
+        try:
+            with open(path, 'rb') as file:
+                content = file.read()
+
+            response = HttpResponse(content, content_type=contentType)
+            response['X-Content-Type-Options'] = "nosniff"
+            #response['Content-Length'] = str(len(content)) is need for very large file, django handles is not
+
+            print(response)
+        
+            return response
+        
+        except OSError as e: # catch most of em, like FileNotFoundError  
+            return HttpResponseNotFound(f"404 Not Found")
+            #return render(request, '404.html', status=404) when make 404 pages 
+
+    else:
+        return HttpResponseNotFound("404 Not Found")
+    
+def addCookies(response, cookies): # add all cookies from the give dic to the response
+    for cookieName, cookieValue in cookies.items():
+        response.set_cookie(cookieName, cookieValue)
+    return response
